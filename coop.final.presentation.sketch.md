edd96d93b3b785f8338b2bb101b563c0d754bc5b
Matt Forster <forster.matth@gmail.com>
2016-06-15 12:19:43 -0600

    Update sketch

--- coop.final.presentation.sketch.md
+++ coop.final.presentation.sketch.md
@@ -1,13 +1,13 @@
-
 # Notes
 
 what makes this unique?
 working in a startup environment
 
--   title: Working For A Startup or: How working for a smaller company scratches
-that itch
+## title
 
-elements:
+Working For a Startup or: How working for a smaller company scratches that itch
+
+## elements
 
 -   overview: working at Autovance
 -   the startup environment
@@ -17,17 +17,28 @@ elements:
 -   outcomes
 -   conclusion
 
-## Presentation Sketch
+## Presentation Notes
 
-### overview
+### objective
 
-talking about my time at Autovance and my ideal workplace
+talking about my time at Autovance and my ideal workplace.
+
+  How did this come to be?
+  Working at such a great place
+  People, challenges
 
 ### the startup environment
 
 -   freedom of choice
+
 -   integral participation
+
 -   highly motivated team environment
+
+    Choices having wide effect on the direction of the company
+    sitting beside and solving problems every day
+    easily brainstorm
+    excitement about challenges and solutions
 
 ### company: the role of an employee in a startup
 
@@ -38,19 +49,27 @@ talking about my time at Autovance and my ideal workplace
 -   environment
 
     -   silicon coulee
+
+        joke
+
     -   what is the 'ideal workplace'
+
+        helping the company and people move forward
+        everybody looking in the same direction
 
 -   the 'feelgood': involvement
 
-    -   not just another cubicle - harsh? not really
-    -   lasting impact on the company
-    -   motivated by being a part of something
+        not just another cubicle - harsh? not really
+        lasting impact on the company
+        motivated by being a part of something
 
 ### learning: from nothing to now (highlights)
 
 -   confidence
+
 -   open source contributions
--   future?
+
+    future?
 
 ### learning objectives
 
@@ -65,7 +84,15 @@ talking about my time at Autovance and my ideal workplace
 
 -   what objectives did I meet
 
+      language, teamwork
+      learnt a lot about javascript ecosystem
+      proficent developer workflow
+
 -   what objectives are still underway?
+
+      High level learning
+      working in a team - code review
+      finding that perfect flow
 
     -   will they ever end? no
 
@@ -73,10 +100,18 @@ talking about my time at Autovance and my ideal workplace
 
 -   finding motivators is important to feeling good about work
 -   motivators
--   ???
+
+       good code - more learning / working smart
+       solving problems - being challenged
+       collaberation, using tools (github, trello)
 
 ### conclusion
 
 -   i've become an integral part of the company
 -   i've come a long way, but the learning is still in full swing
 -   i've become a part of something great
+
+    job: fulfilling
+    gives me room to grow professionally and personally
+
+## Questions
--- presentation/index.js
+++ presentation/index.js
@@ -30,9 +30,6 @@ import preloader from "spectacle/lib/utils/preloader";
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
-// Import custom component
-import Interactive from "../assets/interactive";
-
 // Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
@@ -71,9 +68,9 @@ export default class Presentation extends React.Component {
           <Slide transition={["slide"]} align={"right center"} notes="test">
             <Heading size={1} fit caps lineHeight={1} textColor="heading">The Startup Environment</Heading>
 
-              <Text padding={20} bold textColor="secondary" textAlign={"right"}>freedom of choice</Text>
-              <Text padding={20} textColor="secondary" textAlign={"right"}>integral participation</Text>
-              <Text padding={20} textColor="secondary" textAlign={"right"}>highly motivated team environment</Text>
+              <Text margin="40px 0px 0px" textColor="secondary" textAlign={"right"}>freedom of choice</Text>
+              <Text textColor="secondary" textAlign={"right"}>integral participation</Text>
+              <Text textColor="secondary" textAlign={"right"}>highly motivated team environment</Text>
 
           {/*__·__ _high level descisions left to me_
           __·__ _responsibilities are high_
@@ -87,9 +84,9 @@ export default class Presentation extends React.Component {
           <Slide transition={["slide"]} align={"left center"}>
             <Heading size={1} fit caps textColor="heading">Company: The role of an employee in a startup</Heading>
 
-              <Text padding={20} bold textColor="secondary" textAlign={"left"}>it affects the choices you make</Text>
-              <Text padding={20} textColor="secondary" textAlign={"left"}>environment</Text>
-              <Text padding={20} textColor="secondary" textAlign={"left"}>the 'feelgood': involvement</Text>
+              <Text margin="40px 0px 0px" textColor="secondary" textAlign={"left"}>changed the way I made desicions</Text>
+              <Text textColor="secondary" textAlign={"left"}>environment - friendly and competitive</Text>
+              <Text textColor="secondary" textAlign={"left"}>the 'feelgood': involvement</Text>
 
               {/*__·__ immediate stakeholder
               __·__ silicon coulee?
@@ -120,25 +117,26 @@ export default class Presentation extends React.Component {
           <Slide transition={["slide"]} align={"center center"}>
             <Heading size={1} fit caps lineHeight={1} textColor="secondary">Learning objectives</Heading>
 
-            <Text padding={20} bold textColor="text" textAlign={"center"}>how they changed</Text>
-            <Text padding={20} textColor="text" textAlign={"center"}>evolved from simple to complex</Text>
-            <Text padding={20} bold textColor="text" textAlign={"center"}>my idea of a software job</Text>
-            <Text padding={20} textColor="text" textAlign={"center"}>expectations that I laid upon myself changed</Text>
+            <Text margin="50px 0px 0px" bold textColor="text" textAlign={"center"}>how they changed</Text>
+            <Text textColor="text" textAlign={"center"}>evolved from simple to complex</Text>
+
+            <Text margin="50px 0px 0px" bold textColor="text" textAlign={"center"}>my idea of a software job</Text>
+            <Text textColor="text" textAlign={"center"}>expectations that I laid upon myself changed</Text>
 
           </Slide>
 
           <Slide transition={["slide"]} align={"center center"}>
             <Heading size={1} fit caps lineHeight={1} textColor="secondary">Learning outcomes</Heading>
 
-            <Text bold textColor="text" textAlign={"center"}>some objectives were met</Text>
+            <Text margin="50px 0px 0px" bold textColor="text" textAlign={"center"}>some objectives were met</Text>
             <Text textColor="text" textAlign={"center"}>basic language objectives</Text>
             <Text textColor="text" textAlign={"center"}>basic team objectives</Text>
-            <Text textColor="primary" textAlign={"center"}>fill</Text>
-            <Text bold textColor="text" textAlign={"center"}>most are still underway</Text>
+
+            <Text margin="50px 0px 0px" bold textColor="text" textAlign={"center"}>most are still underway</Text>
             <Text textColor="text" textAlign={"center"}>improving my abilities</Text>
             <Text textColor="text" textAlign={"center"}>improving my teamwork</Text>
-            <Text textColor="primary" textAlign={"center"}>fill</Text>
-            <Text bold textColor="text" textAlign={"center"}>will they ever end?</Text>
+
+            <Text margin="50px 0px 0px" bold textColor="text" textAlign={"center"}>will they ever end?</Text>
             <Text bold textColor="secondary" textAlign={"center"}>no</Text>
 
 
@@ -148,139 +146,25 @@ export default class Presentation extends React.Component {
             <Heading size={1} fit caps lineHeight={1} textColor="heading">My motivators (that itch)</Heading>
             <Text textColor="text" textAlign={"center"}>finding motivators is important to feeling good about work</Text>
 
-              <Text textAlign="center" textColor="primary">filler</Text>
-
-              <Text textAlign="center" textColor="secondary">creating good code</Text>
-              <Text textAlign="center" textColor="secondary">solving problems</Text>
-              <Text textAlign="center" textColor="secondary">improving quality through collaboration</Text>
+              <Text margin="50px 0px 0px" bold textAlign="center" textColor="secondary">creating good code</Text>
+              <Text bold textAlign="center" textColor="secondary">solving problems</Text>
+              <Text bold textAlign="center" textColor="secondary">improving quality through collaboration</Text>
 
           </Slide>
 
           <Slide transition={["slide"]} align={"center center"}>
             <Heading textSize="3em" caps lineHeight={1} textColor="heading">Conclusion</Heading>
-            <Text padding={20} bold textColor="primary" textAlign={"left"}>filler</Text>
+
             <Text bold textColor="secondary" textAlign={"center"}>i've become an integral part of the company</Text>
             <Text textColor="secondary" textAlign={"center"}>have come a long way, but the learning is still in full swing</Text>
             <Text textColor="secondary" textAlign={"center"}>becoming a part of something great</Text>
+
+            <Text> presentation code available at <Link></Link> </Text>
           </Slide>
 
         </Deck>
       </Spectacle>
 
-
-        /*<Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
-            </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
-          </Slide>
-        </Deck>*/
     );
   }
 }
